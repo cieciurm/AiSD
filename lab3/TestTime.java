4f0be5bab2ad09432c831b2386a3bb464d07598b
Mateusz Cieciura <mateusz5000@gmail.com>
2012-10-17 12:02:18 +0200

    Writing to log files

--- lab3/TestTime.java
+++ lab3/TestTime.java
@@ -1,3 +1,8 @@
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -9,16 +14,31 @@
  * 
  */
 public class TestTime {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter out = new BufferedWriter (new FileWriter ("list.adding.log"));
         long t1, t2;
+        int i;
         ListPQueue list = new ListPQueue();
         
-        for(int i = 1; i < 100; i++) {
+        for(i = 1; i < 1000; i++) {
             t1 = System.nanoTime();
             list.insert(i);
             t2 = System.nanoTime();
-            System.err.println(i + " " + (t2-t1));
+            out.write(i + " " + (t2-t1) + "\n");
+            //System.err.println(i + " " + (t2-t1));
         }
+        out.close();
+        
+        BufferedWriter out2 = new BufferedWriter (new FileWriter ("list.removing.log"));
+
+        while (!list.isEmpty()) {
+            t1 = System.nanoTime();
+            list.remove();
+            t2 = System.nanoTime();
+            out2.write(i + " " + (t2-t1) + "\n");
+            i--;
+        }
+        out2.close();
     }
-    
 }
+
