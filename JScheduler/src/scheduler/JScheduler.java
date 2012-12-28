77c004b9b5c13779635f870af8c3707635cf045c
Mateusz Cieciura <mateusz5000@gmail.com>
2012-12-28 17:06:16 +0100

    Added displaying stats to JScheduler

--- JScheduler/src/scheduler/JScheduler.java
+++ JScheduler/src/scheduler/JScheduler.java
@@ -1,39 +1,39 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package scheduler;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
-/**
- *
- * @author mateusz
- */
 public class JScheduler {
   private SchedulerHeap sh;
   private Parser p;
+  private int [] stats;
   
-  public JScheduler(String filename) throws FileNotFoundException, NullPointerException {
-		//try {
+  public JScheduler(String filename) {
+      try {
         this.sh = new SchedulerHeap();
         this.p = new Parser(filename);
-				//} catch (FileNotFoundException e) {
-				//System.out.println("# Error! File not found!");
-				//help();
-				//} 
+      } catch (Exception e) {
+          System.out.println(e.getMessage());
+      } 
+      this.stats = new int [3];
   }
 
-	/* Public so it can be used (displayed) while opening file in Parser.java */
+	/* Public so it could be accessed from Parser.java */
   public static void help() {
       System.out.println ("# ");
       System.out.println ("# Usage:");
-      System.out.println ("# java -classpath <current_dirrectory> src.scheduler.JScheduler -f <file_with_orders>");
+			System.out.println ("# java -classpath <current_dirrectory> src.scheduler.JScheduler -f <file_with_orders>");
       System.out.println ("# ");
   }
+  
+  private void writeStats() {
+      System.out.println("*** Stats: ***");
+      System.out.println("* Added: " + this.stats[0]);
+      System.out.println("* Removed: " + this.stats[1]);
+      System.out.println("* Changed: " + this.stats[2]);
+      System.out.println("******");
+  }
 
-  public static void main(String [] args) throws NullPointerException, IOException {
+  public static void main(String [] args) throws IOException {
       
       if (args.length <= 1) {
           System.out.println("# Error! Too less parameters!");
@@ -47,8 +47,7 @@ public class JScheduler {
           return;
       }
       
-			JScheduler scheduler = new JScheduler(args[1]);
-
+      JScheduler scheduler = new JScheduler(args[1]);
       String s;
       int operation = 0;
       //System.out.println(scheduler.sh.isEmpty());
@@ -59,16 +58,22 @@ public class JScheduler {
               //System.out.println("Adding");
               int [] tmp = scheduler.p.getCache();
               scheduler.sh.add(new Job(tmp[0], tmp[1]));
+              scheduler.stats[0]++;
           } else if (operation == 2) {
               //System.out.println("Removing");
               scheduler.sh.remove();
+              scheduler.stats[1]++;
           } else if (operation == 3) {
               //System.out.println("Changing");
               int [] tmp = scheduler.p.getCache();
               scheduler.sh.changePriority(tmp[0], tmp[1]);
+              scheduler.stats[2]++;
           }
       }
       
       scheduler.sh.writeHeap();
+      scheduler.writeStats();
+      
+
   }
 }
